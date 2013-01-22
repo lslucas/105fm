a9f76228d4242602a900a1dc4837da6a2446e9a4
Lucas Serafim <lslucas@gmail.com>
2013-01-21 21:16:52 -0600

    alterado email do usuário

--- admin/usuario/helper/exec.email.php
+++ admin/usuario/helper/exec.email.php
@@ -6,17 +6,17 @@ if ($act=='insert') {
 	     Olá ".$res['nome'].", abaixo seus dados de acesso ao ".SITE_NAME.":
 	     <p><b>Login:</b> ".$res['email']."
 	     <br><b>Senha:</b> <code>".$res['senha']."</code>
-	     <br><b>Painel:</b> <a href='".PAINEL_URL."' target='_blank'>".PAINEL_URL."</a>
+	     <br><b>Painel:</b> <a href='".SITE_URL."login' target='_blank'>".SITE_URL."login</a>
 
 	     <p>Você pode alterar sua senha a qualquer hora!</p>";
 
 } elseif ($act=='update') {
-    $email_subject = utf8_decode(SITE_NAME.": Informações de conta alterada");
+    $email_subject = utf8_encode(SITE_NAME.": Informações de conta alterada");
     $msg .= "
 	     Olá ".$res['nome'].", abaixo seus dados de acesso ao ".SITE_NAME.":
 	     <p><b>Login:</b> ".$res['email']."
 	     <br><b>Senha:</b> ".(empty($res['senha']) ? 'continua a mesma!' : '<code>'.$res['senha'].'</code>')."
-	     <br><b>Painel:</b> <a href='".PAINEL_URL."' target='_blank'>".PAINEL_URL."</a>
+	     <br><b>Painel:</b> <a href='".SITE_URL."login' target='_blank'>".SITE_URL."login</a>
 
 	     <p>Você pode alterar sua senha a qualquer hora!</p>";
 
