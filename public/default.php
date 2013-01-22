2ed2f18df0cd42e2ce6f975ae12731d4c96799e4
Lucas Serafim <lslucas@gmail.com>
2013-01-22 14:20:40 -0600

    na home, links de comprar/vender tem destinos diferentes se estiver logado, senao vai sempre pro /login

--- public/default.php
+++ public/default.php
@@ -18,12 +18,20 @@
 			<div class="row produtos">
 				<div style='float:left; width:265px; height:325px; background-image: url("<?=STATIC_PATH?>layout/soybeanplant.jpg"); text-align:center; display:table; position:relative'>
 					<div style='display:table-cell; vertical-align:middle; '>
+						<?php if (isset($usr['id']) && !empty($usr['id'])) { ?>
+						<a href='<?=ABSPATH?>lista' class='btn-agro'>Comprar</a>
+						<?php } else { ?>
 						<a href='<?=ABSPATH?>login' class='btn-agro'>Comprar</a>
+						<?php } ?>
 					</div>
 				</div>
 				<div style='float:left; width:265px; height:325px; background-image: url("<?=STATIC_PATH?>layout/coffeebean.jpg"); margin-left:6px; text-align:center; display:table; position:relative'>
 					<div style='display:table-cell; vertical-align:middle; '>
+						<?php if (isset($usr['id']) && !empty($usr['id'])) { ?>
+						<a href='<?=ABSPATH?>novo-produto' class='btn-agro'>Vender</a>
+						<?php } else { ?>
 						<a href='<?=ABSPATH?>login' class='btn-agro'>Vender</a>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
