c6cbea4aa32372f19190865b1565c334199b13c3
Lucas Serafim <lslucas@gmail.com>
2013-01-28 09:11:52 -0600

    anuncie

---
+++ public/anuncie.php
@@ -0,0 +1,4 @@
+<div class="column grid_10">
+    <h1>Anuncie</h1>
+    <br/><p>Para anunciar, entre em contato com <a href='mailto:vendas@agrosshop.com.br'>vendas@agrosshop.com.br</a> informando telefone e empresa.</p>
+</div>
