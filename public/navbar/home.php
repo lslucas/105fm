7dd49205f385249e8b538030d1f4b60fa0c2f6b4
Lucas Serafim <lslucas@gmail.com>
2013-01-28 12:04:49 -0600

    adicionado title

--- public/navbar/home.php
+++ public/navbar/home.php
@@ -15,19 +15,19 @@
 					<h4>Cotações</h4>
 					<ul>
 						<li style='width:100%; height:50px; display:table; position:relative;'>
-							<img src='<?=STATIC_PATH?>assets/dolar.png' class='pull-left' border=0/>
+							<img src='<?=STATIC_PATH?>assets/dolar.png' class='pull-left' title='Dolar' border=0/>
 							<div style='display: table-cell; vertical-align:middle; padding-left:20px; width:100%;  '>
 								R$ <?=cotacao()?>
 							</div>
 						</li>
 						<li style='width:100%; height:50px; display:table; position:relative;'>
-							<img src='<?=STATIC_PATH?>assets/euro.png' class='pull-left' border=0/>
+							<img src='<?=STATIC_PATH?>assets/euro.png' class='pull-left' title='Euro' border=0/>
 							<div style='display: table-cell; vertical-align:middle; padding-left:20px; width:100%;  '>
 								R$ <?=cotacao('EUR')?>
 							</div>
 						</li>
 						<li style='width:100%; height:50px; display:table; position:relative;'>
-							<img src='<?=STATIC_PATH?>assets/libra.png' class='pull-left' border=0/>
+							<img src='<?=STATIC_PATH?>assets/libra.png' class='pull-left' title='Libra' border=0/>
 							<div style='display: table-cell; vertical-align:middle; padding-left:20px; width:100%;  '>
 								R$ <?=cotacao('GBP')?>
 							</div>
