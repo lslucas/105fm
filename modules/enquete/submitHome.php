9e570346f255cae50b5a9d9373fceca68b6cc9a3
Lucas Serafim <lslucas@gmail.com>
2013-08-05 10:34:32 -0500

    permitir apenas um voto por ip na enquete

--- modules/enquete/submitHome.php
+++ modules/enquete/submitHome.php
@@ -15,19 +15,35 @@
     if (!isset($enq_id) || !isset($res) || !in_array($res, array(1, 2, 3, 4, 5)))
         echo "Ops, você fez algo de errado, senão tente mais tarde!";
 
-    /**
-     * Após validado insere voto e mostra estatistica de resostas
-     */
-    $sql_enq = "INSERT INTO ".TP."_enquete_votos (enq_id, res, ip) VALUES (?, ?, ?)";
-    if (!$qry_enq = $conn->prepare($sql_enq))
+    $sql = "SELECT NULL FROM ".TP."_enquete_votos WHERE enq_id=? AND ip=?"; 
+    if (!$qry = $conn->prepare($sql))
         echo $conn->error();
 
     else {
-        $qry_enq->bind_param('iis', $enq_id, $res, $_SERVER['REMOTE_ADDR']);
-        $qry_enq->execute();
-        $qry_enq->close();
+        $qry->bind_param('is', $enq_id, $_SERVER['REMOTE_ADDR']); //$resposta,
+        $qry->execute();
+        $qry->store_result();
+        $num = $qry->num_rows;
+        $qry->close();
 
-        echo "<br/><b>Voto computado!</b><p>";
+        if ($num==0) {
+
+            /**
+             * Após validado insere voto e mostra estatistica de resostas
+             */
+            $sql_enq = "INSERT INTO ".TP."_enquete_votos (enq_id, res, ip) VALUES (?, ?, ?)";
+            if (!$qry_enq = $conn->prepare($sql_enq))
+                echo $conn->error();
+
+            else {
+                $qry_enq->bind_param('iis', $enq_id, $res, $_SERVER['REMOTE_ADDR']);
+                $qry_enq->execute();
+                $qry_enq->close();
+
+                echo "<br/><b>Voto computado!</b><p>";
+            }
+        } else
+            echo "<br/><b>Você já votou nessa enquete!</b><p>";
 
         /** resgata todas as perguntas **/
         $sqlpenq = "SELECT enq_titulo, enq_res1, enq_res2, enq_res3, enq_res4, enq_res5 FROM ".TP."_enquete WHERE enq_id=?";
