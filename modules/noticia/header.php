73135aa66d86e343fa46f88577b87e5d2af89c5a
Lucas Serafim <lslucas@gmail.com>
2013-09-02 11:15:20 -0500

    correção share facebook

--- modules/noticia/header.php
+++ modules/noticia/header.php
@@ -14,52 +14,55 @@
 
 
 
-    	/**
-    	 * Resgata ID da última noticia cadastrada para caso não seja informado um  id de noticia
-    	 * @var string
-    	 */
+        /**
+         * Resgata ID da última noticia cadastrada para caso não seja informado um  id de noticia
+         * @var string
+         */
             $sql_not = "SELECT not_id FROM ".TABLE_PREFIX."_noticia WHERE not_status=1 ORDER BY not_data DESC LIMIT 1";
             if ($qry_not = $conn->prepare($sql_not)) {
-	            $qry_not->bind_result($item);
-	            $qry_not->execute();
-	            $qry_not->fetch();
-	            $qry_not->close();
-	}
+                $qry_not->bind_result($item);
+                $qry_not->execute();
+                $qry_not->fetch();
+                $qry_not->close();
+    }
 
-	$list  = array();
+    $list  = array();
 
-	/*
-	 *Lista posts de acordo com configuração
-	 */
-	$sqlpos =  "SELECT
-					not_id,
-					not_titulo,
-					not_texto,
-					DATE_FORMAT(not_data, '%d/%m/%Y') `data`,
-					(SELECT rng_imagem FROM ".TP."_r_noticia_galeria WHERE rng_not_id=not_id ORDER BY rng_pos LIMIT 1)
-				FROM ".TP."_noticia
-				WHERE not_status=1
-				AND not_id=?
-			";
-	 if (!$qrypos=$conn->prepare($sqlpos))
-		 echo "<div class='alert alert-error'>".$conn->error."</div>";
+    /*
+     *Lista posts de acordo com configuração
+     */
+    $sqlpos =  "SELECT
+                    not_id,
+                    not_titulo,
+                    not_texto,
+                    not_resumo,
+                    DATE_FORMAT(not_data, '%d/%m/%Y') `data`,
+                    (SELECT rng_imagem FROM ".TP."_r_noticia_galeria WHERE rng_not_id=not_id ORDER BY rng_pos LIMIT 1)
+                FROM ".TP."_noticia
+                WHERE not_status=1
+                AND not_id=?
+            ";
+     if (!$qrypos=$conn->prepare($sqlpos))
+         echo "<div class='alert alert-error'>".$conn->error."</div>";
 
-	 else {
+     else {
 
-	 	if (!empty($querystring)) {
-		 	$item = $hashids->decrypt($querystring);
-		 	$item = isset($item[0]) ? $item[0] : null;
-		}
+        if (!empty($querystring)) {
+            $item = $hashids->decrypt($querystring);
+            $item = isset($item[0]) ? $item[0] : null;
+        }
 
-		$qrypos->bind_param('i', $item);
-		$qrypos->bind_result($id, $titulo, $texto, $data, $imagem);
-		$qrypos->execute();
-		$qrypos->store_result();
-		$qrypos->fetch();
-		$total = $qrypos->num_rows;
-		$qrypos->close();
+        $qrypos->bind_param('i', $item);
+        $qrypos->bind_result($id, $titulo, $texto, $resumo, $data, $imagem);
+        $qrypos->execute();
+        $qrypos->store_result();
+        $qrypos->fetch();
+        $total = $qrypos->num_rows;
+        $qrypos->close();
 
-		$texto = stripslashes($texto);
-		$imagem = !empty($imagem) ? ABSPATH.'images/noticia/'.$imagem : null;
-	}
+        $texto = stripslashes($texto);
+        $imagem = !empty($imagem) ? ABSPATH.'images/noticia/'.$imagem : null;
+        $imagemUrl = !empty($imagem) ? SITE_URL.'images/noticia/'.$imagem : null;
+        $url = SITE_URL.'noticia/' . $hashids->encrypt($id) . '/' .linkfySmart($titulo);
+    }
 
--- public/noticia.php
+++ public/noticia.php
@@ -1,64 +1,23 @@
                     <!--Colunas-->
                     <div id="centro" class="clearfix">
-                        <?php /* ?>
-                        <div id="banner-wrapper">
-                            <!--Banner Slider-->
-                            <div id="slider" style="display:inline-block;">
-                                <div id="RadioBanner" class="">
-                                    <img src="<?=ABSPATH?>img/banners/noticias.jpg" width="968" height="385" alt="Notícias da 105 FM" title="Notícias da 105 FM" />
-                                    <div class="SlideDescription">Notícias da 105 FM</div>
-
-                                    <img src="<?=ABSPATH?>img/banners/noticias.jpg" width="968" height="385" alt="Notícias da 105 FM" title="Notícias da 105 FM" />
-                                    <div class="SlideDescription">Notícias da 105 FM</div>
-
-                                    <img src="<?=ABSPATH?>img/banners/noticias.jpg" width="968" height="385" alt="Notícias da 105 FM" title="Notícias da 105 FM" />
-                                    <div class="SlideDescription">Notícias da 105 FM</div>
-
-                                    <img src="<?=ABSPATH?>img/banners/noticias.jpg" width="968" height="385" alt="Notícias da 105 FM" title="Notícias da 105 FM" />
-                                    <div class="SlideDescription">Notícias da 105 FM</div>
-
-                                </div>
-                                <div class="c"></div>
-                                <div id="RadioNav"></div>
-                                <div class="c"></div>
-
-                                <script type="text/javascript">
-                                    <!--
-                                    efeitos = 'rain,stairs,fade';
-                                    var demoSlider_2 = Sliderman.slider({container: 'RadioBanner', width: 968, height: 385, effects: efeitos,
-                                        display: {
-                                            autoplay: 7000,
-                                            loading: {background: '#ffffff', opacity: 0.5, image: '<?=ABSPATH?>img/loading.gif'},
-                                            buttons: {hide: true, opacity: 1, prev: {className: 'RadioPrev', label: ''}, next: {className: 'RadioNext', label: ''}},
-                                            description: {hide: true, background: '#000000', opacity: 0.4, height: 50, position: 'bottom'},
-                                            navigation: {container: 'RadioNav', label: '<img src="<?=ABSPATH?>img/clear.gif" />'}
-                                        }
-                                    });
-                                    //-->
-                                </script>
-
-                                <div class="c"></div>
-                            </div>
-                            <!--End Banner-->
-                           */ ?>
-                            <div class="clear"></div>
-                        </div>
+                        <div class="clear"></div>
+                    </div>
                         <!--End Banner Wrapper-->
                         <div class="clear"></div>
 
                         <div id="noticia-teste" class="box noticia">
-                        	<h1 class="uppercase green" style="margin-left:10px;"><?=$titulo?> <span class="data"><?=$data?></span></h1>
+                            <h1 class="uppercase green" style="margin-left:10px;"><?=$titulo?> <span class="data"><?=$data?></span></h1>
                             <div class="conteudo-noticia">
                                 <p><?=$texto?></p>
                             </div>
                             <div class="galeria-noticia">
-                            	<div class="img-noticia">
-                                	<img src="<?=$imagem?>" alt="<?=$titulo?>" />
+                        	<div class="img-noticia">
+                        	<img src="<?=$imagem?>" alt="<?=$titulo?>" />
                                 </div>
                                 <ul class="util">
-                                	<li class="print"><a href="javascript:document.print();" title="Imprima esta notícia" onclick="window.print();">Imprimir</a></li>
+                                    <li class="print"><a href="javascript:document.print();" title="Imprima esta notícia" onclick="window.print();">Imprimir</a></li>
                                     <!-- <li class="send"><a href="enviar.html" title="Envie para um amigo" class="fancy">Enviar para um amigo</a></li> -->
-                                    <li class="share"><a href="http://www.facebook.com/sharer.php?u='+ document.location.href + '&t='+ document.title;+'" target="blank">Compartilhe</a></li>
+                                    <li class="share"><a href="javascript:postToFeed(); return false;">Compartilhe</a></li>
                                     <?php /* ?>
                                     <li class="prev"><a href="<?=$prev['link']?>" title="Notícia Anterior">Anterior</a></li>
                                     <li class="next"><a href="<?=$next['link']?>" title="Próxima Notícia">Próxima</a></li>
@@ -81,3 +40,25 @@
 
                     </div>
                     <!--End Centro-->
+ <script src='http://connect.facebook.net/en_US/all.js'></script>
+<script>
+  FB.init({appId: "496705820402963", status: true, cookie: true});
+  function postToFeed() {
+    // calling the API ...
+    var obj = {
+      method: 'feed',
+      redirect_uri: '<?=$url?>',
+      link: '<?=SITE_URL?>',
+      picture: '<?=SITE_URL.$imagem?>',
+      name: '<?=$titulo?>',
+      caption: '',
+      description: '<?=$resumo?>'
+    };
+
+    function callback(response) {
+      document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+    }
+
+    FB.ui(obj, callback);
+  }
+</script>
