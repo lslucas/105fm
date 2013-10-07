36ae82d836bf6575e5c7a8630c1ab1b3e7ba159a
Lucas Serafim <lslucas@gmail.com>
2013-10-07 13:01:29 -0500

    testes

--- admin/noticia/helper/exec.galeria.php
+++ admin/noticia/helper/exec.galeria.php
@@ -73,7 +73,7 @@
 		$handle->image_resize        = true;
 		// $handle->image_ratio = true;
 		// $handle->image_ratio_crop    = true;
-                      // $handle->image_ratio_fill    = '#BBC4D9';
+                      // $handle->image_ratio_fill    = '#BACEDB';
                       // $handle->image_ratio_no_zoom_in = true;
 		$handle->image_x             = $var['homeWidth'];
 		$handle->image_y             = $var['homeHeight'];
